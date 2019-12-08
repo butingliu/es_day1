6d8c3202ff043f95f629a82d36c350495e2a0f27
yaya <1411145689@qq.com>
2019-12-08 10:16:52 +0800

    添加UserDao

--- src/main/java/com/baizhi/dao/UserDao.java
+++ src/main/java/com/baizhi/dao/UserDao.java
@@ -7,7 +7,6 @@ public interface UserDao {
     //查询一个
     User selectByNameAndWord(@Param("username") String username,
                              @Param("password") String password);
-
     //添加用户
     void addUser(User user);
 }
